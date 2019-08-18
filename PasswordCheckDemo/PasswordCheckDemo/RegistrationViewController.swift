8fddb039562cad39800b2f8bb849f8ad3f654245
Kristijan Kralj <kristijan.kralj@live.com>
2019-08-18 17:57:12 +0200

    Add simple registration form

---
+++ PasswordCheckDemo/PasswordCheckDemo/RegistrationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PasswordCheckDemo
+//
+//  Created by Kristijan Kralj on 18/08/2019.
+//  Copyright © 2019 Kristijan Kralj. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
